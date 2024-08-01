dc3246e5532e1c4e2adafb6733a9e63b493a1dd7
Cheng Qian <cheng.qian@tum.de>
2024-08-01 09:08:41 +0000

    Change ckpt path

--- scripts/eval_low_level.sh
+++ scripts/eval_low_level.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
+# Check if the song name is given
+if [ -z "$1" ]; then
+  echo "No song name given"
+  exit 1
+fi
 
-WANDB_DIR=/tmp/robopianist/ MUJOCO_GL=egl XLA_PYTHON_CLIENT_PREALLOCATE=false CUDA_VISIBLE_DEVICES=0 MUJOCO_EGL_DEVICE_ID=0 python pianomime/multi_task/eval_low_level.py \
-    --root-dir ./diffusion/eval_videos/ \
-    --warmstart-steps 5000 \
-    --max-steps 1000000 \
-    --discount 0.99 \
-    --trim-silence \
-    --gravity-compensation \
-    --control-timestep 0.05 \
-    --n-steps-lookahead 10 \
-    --tqdm-bar \
-    --action-reward-observation \
-    --eval-episodes 1 \
-    --camera-id "piano/back" \
+# Capture the argument
+ARGUMENT=$1
+WANDB_DIR=/tmp/robopianist/ MUJOCO_GL=egl XLA_PYTHON_CLIENT_PREALLOCATE=false CUDA_VISIBLE_DEVICES=0 MUJOCO_EGL_DEVICE_ID=0 python pianomime/multi_task/eval_low_level.py $ARGUMENT
